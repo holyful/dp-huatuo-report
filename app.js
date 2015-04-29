60cacfa007869c8326a27840cfbedef927af856d
Daxuan She <holyful@hotmail.com>
2015-04-29 14:46:21 +0800

    for test

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var APPKEY = "9WQx7JInsjShOvRGNLb61w==";
+
+
+app.get('/app/:id', function(req, res) {
+   
+	request('http://huatuo.qq.com/Openapi/AppSpeedConfigList?appkey='+APPKEY+'&appId='+req.params.id,function(error,response,body){
+		console.log(response)
+	})
+
+});
