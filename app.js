c50c0d96ce05685ce684390873dd5db10013b5ff
Daxuan She <holyful@hotmail.com>
2015-05-06 15:31:50 +0800

    new api

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var api = require('./routes/api');
-
 var app = express();
 
 // view engine setup
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ var request = require('request');
 var querystring = require('querystring');
 var _ = require('underscore');
 var APPKEY = "9WQx7JInsjShOvRGNLb61w=="
+var MOCK_APP_PATH = "../mock/app.json";
 var urlOptions = {
 	useQuerystring : true,
 	baseUrl: "http://huatuo.qq.com/Openapi/"
@@ -11,9 +12,12 @@ var urlOptions = {
 var Memcached = require('memcached');
 var memcached = new Memcached('192.168.7.94:11211');
 var Util = require('../lib/util');
-
+var DEBUG = false;
+var MOCK = false;
 
 var appHandler = function(req, res, next){
+	var DEBUG = !!(req.query.debug * 1) || false;
+	var MOCK = !!(req.query.mock * 1) || false;
 	var apiOption = {
 		uri:"AppSpeedConfigList",
 		qs: {
@@ -23,72 +27,180 @@ var appHandler = function(req, res, next){
 	}
 
 	var gapTimestamp = 3600000; //mil seconds
-	var siteId = req.params.siteId || req.params.siteId * 1;
-	var subId = req.params.subId || req.params.subId * 1;
-	var pageId = req.params.pageId || req.params.pageId * 1;
-	var pointId = req.params.pointId || req.params.pointId * 1;
+	var siteId = req.query.siteId || req.query.siteId * 1;
+	var subId = req.query.subId || req.query.subId * 1;
+	var pageId = req.query.pageId || req.query.pageId * 1;
+	var pointId = req.query.pointId || req.query.pointId * 1;
+	var siteName = req.query.siteName;
+	var pageName = req.query.pageName;
+	var eventName = req.query.eventName;
+	var allTag = siteId || subId || pageId || pointId || siteName || pageName || eventName;
 	var memcacheKey = [apiOption.uri,apiOption.qs.appId,Math.floor(Date.now()/gapTimestamp)].join('@');
-	var handler = function(data,siteId,subId,pageId,pointId){
+	var handler = function(data){
 		var result = {
 			data : []
 		};
-		if(!data){
-			return;
+		if(!data.data){
+			return {
+				'error' : data.msg
+			};
 		}
 
-		data.forEach(function(siteData){
+
+		result.data = data.data.filter(function(siteData){
 			if(siteId){
 				if(siteData[0].indexOf(siteId + '-') >= 0){
-					if(subId){
-						if(siteData[0].indexOf(siteId + '-'+ subId + '-') >= 0){
-							if(pageId){
-								if(siteData[0].indexOf(siteId + '-'+ subId + '-' + pageId + '-') >= 0){
-									if(pointId){
-										if(siteData[0] === siteId + '-'+ subId + '-' + pageId + '-' + pointId){
-											result.data.push(siteData);
-										}
-										return;
-									}
-									result.data.push(siteData);
-								}
-								return;
-							}
-							result.data.push(siteData);
-						}
-						return;
-					}
-					result.data.push(siteData);
+					return true;
+				}else{
+					return false;
 				}
-				return;
+			}else{
+				return true;
 			}
-			result.data.push(siteData);
-		})
+		}).filter(function(siteData){
+			if(siteId && subId){
+				if(siteData[0].indexOf(siteId + '-'+ subId + '-') >= 0){
+					return true;	
+				}else{
+					return false;
+				}
+			}else{
+				return true;
+			}
+
+		}).filter(function(siteData){
+			if(siteId && subId && pageId){
+				if(siteData[0].indexOf(siteId + '-'+ subId + '-' + pageId + '-') >= 0){
+					return true;
+				}else{
+					return false;
+				}
+			}else{
+				return true;
+			}
+		}).filter(function(siteData){
+			if(pointId){
+				if(siteData[0].lastIndexOf( '-' + pointId) === (siteData[0].length - ('-' + pointId).length)){
+					return true;
+				}else{
+					return false;
+				}
+			}else{
+				return true;
+
+			}
+		}).filter(function(siteData){
+			if(siteName){
+				if(siteData[1].indexOf(siteName) >= 0) {
+					return true;
+				}else{
+					return false;
+				}
+			}else{
+				return true;
+			}
+		}).filter(function(siteData){
+			if(pageName){
+				if(siteData[2].indexOf(pageName) >= 0) {
+					return true;
+				}else{
+					return false;
+				}
+			}else{
+				return true;
+			}
+		}).filter(function(siteData){
+			if(eventName){
+				if(siteData[3] === eventName) {
+					return true;
+				}else{
+					return false;
+				}
+			}else{
+				return true;
+			}
+		});
 
 		return JSON.stringify(result);
 
 	};
+
+
 	memcached.get(memcacheKey, function (err, data) {
 		res.setHeader("Content-Type","application/json");
-		if(!data){
+		if(!data || DEBUG){
 			res.setHeader("Cache","MISS");
-			request(_.extend(urlOptions,apiOption),function(error, response, body){
-				memcached.set(memcacheKey, body, gapTimestamp/1000, function(){})
-
-					res.send(handler(Util.getResult(body).data, siteId, subId, pageId, pointId));
-
+			if(!MOCK){
+				request(_.extend(urlOptions,apiOption),function(error, response, body){
+					if(!DEBUG){
+						memcached.set(memcacheKey, body, gapTimestamp/1000, function(){})
+					}
+					res.send(handler(Util.getResult(body)));
 				});
+				
+			}else{
+				res.send(handler(require(MOCK_APP_PATH)));
+			}
 			return;
+			
 		}
 		res.setHeader("Cache","HIT");
-		res.send(handler(Util.getResult(data).data, siteId, subId, pageId, pointId));
+		res.send(handler(Util.getResult(data)));
 	});
 
 }
 
+var speedDateHandler = function(req, res, next){
+	var DEBUG = !!(req.query.debug * 1) || false;
+	var MOCK = !!(req.query.mock * 1) || false;
+	var apiOption = {
+		uri:"GetSpeedData",
+		qs: {
+			appId: req.params.appId,
+			format: 'date',
+			flag1: req.params.siteId,
+			flag2: req.params.subSiteId,
+			flag3: req.params.pageId,
+			pointId: req.params.pointId,
+			appkey: APPKEY
+		}
+	}
+
+	var gapTimestamp = 1000; 
+	var memcacheKey = [apiOption.uri,querystring.stringify(apiOption.qs),Math.floor(Date.now()/gapTimestamp)].join('@');
+
+	memcached.get(memcacheKey, function (err, data) {
+		res.setHeader("Content-Type","application/json");
+		if(!data || DEBUG){
+			res.setHeader("Cache","MISS");
+			if(!MOCK){
+				request(_.extend(urlOptions,apiOption),function(error, response, body){
+					if(!DEBUG){
+						memcached.set(memcacheKey, body, gapTimestamp/1000, function(){})
+					}
+					res.send(Util.getResult(body));
+				});
+				
+			}else{
+				res.send(require(MOCK_APP_PATH));
+			}
+			return;
+			
+		}
+		res.setHeader("Cache","HIT");
+		res.send(handler(Util.getResult(data)));
+	});
+
+}
 //http://xili.huatuo.qq.com/Openapi/AppSpeedConfigList?appId=20001&appkey=123456789
-router.get('/app/:id', function(req, res, next) {
+router.get('/info/app/:id', function(req, res, next) {
 	appHandler(req, res, next)
 });
+http://xili.huatuo.qq.com/Openapi/GetSpeedData?appkey=1234567890&format=datesection&flag1=1470&flag2=120&appId=20001&flag3=10&pointId=1&startDate=2015-04-16
+router.get('/speed/date/app/:appId/site/:siteId/sub/:subSiteId/page/:pageId/point/:pointId', function(req, res, next) {
+	speedDateHandler(req, res, next)
+});
+
 
 
 
